8c97df4b7e70dcf0a00fbdb723febd002d764aeb
hao.kou <kouhao2016@126.com>
2019-07-03 16:26:13 +0800

    baocun

---
+++ src/main/java/com/example/configserver/KafkaConsumer.java
@@ -0,0 +1,19 @@
+package com.example.configserver;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class KafkaConsumer {
+
+    @KafkaListener(topics = {"test"})
+    public void consumer(ConsumerRecord<?,?> consumerRecord){
+        Optional<?> kafkaMessage = Optional.ofNullable(consumerRecord.value());
+        if(kafkaMessage.isPresent()){
+            System.out.println("config-server"+kafkaMessage.get());
+        }
+    }
+}
