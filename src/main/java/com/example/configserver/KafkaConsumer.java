098b7a4fe878e30d2458ad59be62eecd412c00d4
kouhao <kouhao2016@126.com>
2019-08-12 17:26:41 +0800

    add sevurity

--- src/main/java/com/example/configserver/KafkaConsumer.java
+++ src/main/java/com/example/configserver/KafkaConsumer.java
@@ -1,7 +1,7 @@
 package com.example.configserver;
 
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.springframework.kafka.annotation.KafkaListener;
+//import org.apache.kafka.clients.consumer.ConsumerRecord;
+//import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -9,11 +9,11 @@ import java.util.Optional;
 @Component
 public class KafkaConsumer {
 
-    @KafkaListener(topics = {"test"})
-    public void consumer(ConsumerRecord<?,?> consumerRecord){
-        Optional<?> kafkaMessage = Optional.ofNullable(consumerRecord.value());
-        if(kafkaMessage.isPresent()){
-            System.out.println("config-server"+kafkaMessage.get());
-        }
-    }
+//    @KafkaListener(topics = {"test"})
+//    public void consumer(ConsumerRecord<?,?> consumerRecord){
+//        Optional<?> kafkaMessage = Optional.ofNullable(consumerRecord.value());
+//        if(kafkaMessage.isPresent()){
+//            System.out.println("config-server"+kafkaMessage.get());
+//        }
+//    }
 }
